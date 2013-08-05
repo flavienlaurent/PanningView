0a65ace4a35c6d2451df6294c256329242d31cfa
flavienlaurent <flavien.laurent@gmail.com>
2013-08-05 18:48:20 +0200

    first commit

---
+++ PanningView/src/main/java/com/fourmob/panningview/MainActivity.java
@@ -0,0 +1,45 @@
+package com.fourmob.panningview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	private static final String TAG = "MainActivity";
+
+	private static final int[] drawables = new int[] {R.drawable.bg_default_artist_art, R.drawable.bg_default_artist_art2, R.drawable.bg_default_artist_art3, R.drawable.bg_default_artist_art4};
+
+	private int mDrawableIndex = 0;
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+		final PanningView panningView = (PanningView) findViewById(R.id.panningView);
+
+		findViewById(R.id.buttonStart).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				panningView.startPanning();
+			}
+		});
+		findViewById(R.id.buttonStop).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				panningView.stopPanning();
+			}
+		});
+		findViewById(R.id.buttonChange).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				mDrawableIndex++;
+				if(mDrawableIndex >= drawables.length) {
+					mDrawableIndex = 0;
+				}
+				panningView.setImageResource(drawables[mDrawableIndex]);
+			}
+		});
+	}
+}
---
+++ README.md
@@ -0,0 +1,39 @@
+PanningView
+==================
+
+PanningView is a library which implements the animated background in the now playing screen of the Play Music app.
+
+![Example Image][1]
+
+Try out the sample APK [here][2]
+
+Including in your project
+-------------------------
+
+Just add the library to your application as a library project.
+
+Usage
+---------
+
+You can see a basic usage [here][3]
+
+License
+-----------
+
+    Copyright 2013 Flavien Laurent
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+  
+ [1]: https://raw.github.com/flavienlaurent/PanningView/master/graphics/PanningView.gif
+ [2]: https://github.com/flavienlaurent/PanningView/blob/master/PanningView-debug-unaligned.apk
+ [3]: https://github.com/flavienlaurent/PanningView/tree/master/PanningView
